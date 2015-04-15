0379915a77047e256a3eeb80b09fe1d60fd3d70f
tom--bo <otsuka.t.2013@gmail.com>
2015-04-15 15:55:56 +0900

    randのseed, マジックナンバー解決

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
+	public static final int AGENT_NUM = 10;
+	private static Random random = new Random(1000000009);
+
 	public static void main(String[] args) {
         List<Agent> agents = new ArrayList<Agent>();
-        for(int i = 0; i<10; i++) {
-        	agents.add(new Agent((int)(Math.random() * 9) +1));
+
+        for(int i = 0; i<AGENT_NUM; i++) {
+        	agents.add(new Agent(random.nextInt(10)+1));
 		}
 		for(Agent agent:agents) {
 			agent.hello();
